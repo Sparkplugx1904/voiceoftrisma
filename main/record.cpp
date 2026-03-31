2eefb608670537c839596053c118d81ff1afc1c7
Sparkplugx1904 <anandapradnyana68@gmail.com>
2026-03-31 15:29:29 +0800

    push

---
+++ main/record.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print_color(string type, string text){
+    if(type == "ERR_COLOR"){
+
+    } else if (type == "WARN_COLOR"){
+
+    } else if (type == "INFO_COLOR"){
+
+    } else if (type == "UP_COLOR")
+    
+}
+
+int main() {
+    
+    return 0;
+}
